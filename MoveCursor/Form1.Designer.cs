13b3c218fa81e46563d19308417f132e552e6d58
Ophis <cyntech@outlook.com>
2019-06-17 12:46:50 +0800

    Added the application that macro the three button that is essential for Star WARS.

---
+++ MoveCursor/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MoveCursor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(130, 121);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(72, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Main Window";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(14, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Popup";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(14, 70);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(67, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Close Popup";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(92, 23);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(51, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(149, 23);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(51, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(92, 45);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(51, 20);
+			this.textBox3.TabIndex = 3;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(92, 67);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(51, 20);
+			this.textBox5.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(149, 45);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(51, 20);
+			this.textBox4.TabIndex = 4;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(149, 67);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(51, 20);
+			this.textBox6.TabIndex = 6;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(15, 124);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(15, 108);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(64, 13);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Interval (ms)";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 157);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(217, 22);
+			this.statusStrip1.TabIndex = 8;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(158, 17);
+			this.toolStripStatusLabel1.Text = "F1 to Start, F2 for Coordinate";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(112, 7);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(12, 13);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "x";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(170, 6);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(12, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "y";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(217, 179);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "NTU STARS CLICKER";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+	}
+}
+
---
+++ MoveCursor/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MoveCursor
+{
+	public partial class Form1 : Form
+	{
+
+		// Mouse Hook Variables
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+		private const int MOUSEEVENT_LEFTDOWN = 0x02;
+		private const int MOUSEEVENT_LEFTUP = 0x04;
+		private const int MOUSEEVENT_MIDDLEDOWN = 0x20;
+		private const int MOUSEEVENT_MIDDLEUP = 0x40;
+		private const int MOUSEEVENT_RIGHTDOWN = 0x08;
+		private const int MOUSEEVENT_RIGHTUP = 0x10;
+
+		// Keybaord Hook Variables
+		const int WH_KEYBOARD_LL = 13; // Number of global LowLevel- hook on the keyboard
+		const int WM_KEYDOWN = 0x100; // Messages pressing
+
+		private LowLevelKeyboardProc _proc;
+
+		private static IntPtr hhook = IntPtr.Zero;
+
+		private bool locateEnable = false;
+		private Timer cursorPointTimer = new Timer();
+		private Timer botTimer = new Timer();
+
+
+		public Form1()
+		{
+			_proc = hookProc;
+			InitializeComponent();
+		}
+
+		// Keyboard Hook Methods
+
+		// GLOBAL HOOK
+		[DllImport("user32.dll")]
+		static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+
+		[DllImport("user32.dll")]
+		static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+		[DllImport("user32.dll")]
+		static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+		[DllImport("kernel32.dll")]
+		static extern IntPtr LoadLibrary(string lpFileName);
+
+		private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+		public void SetHook()
+		{
+			IntPtr hInstance = LoadLibrary("User32");
+			hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+		}
+
+		public void UnHook()
+		{
+			UnhookWindowsHookEx(hhook);
+		}
+
+		public IntPtr hookProc(int code, IntPtr wParam, IntPtr lParam)
+		{
+			if (code >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+			{
+				int vkCode = Marshal.ReadInt32(lParam);
+
+				if (vkCode.ToString() == "112") //162 is ASCI CTRL
+				{
+					startLooping();
+				}
+				else if(vkCode.ToString() == "113")
+				{
+					toolStripStatusLabel1.Text = String.Format("x:{0}, y:{1}", System.Windows.Forms.Control.MousePosition.X.ToString(), System.Windows.Forms.Control.MousePosition.Y.ToString());
+				}
+			}
+			return CallNextHookEx(hhook, code, (int)wParam, lParam);
+		}
+
+		// End of keybaord Hook
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (button1.Text == "Start")
+			{
+				botTimer.Interval = Int32.Parse(textBox7.Text);
+				botTimer.Tick += startClicker;
+				button1.Text = "Stop";
+				botTimer.Enabled = true;
+			}
+			else
+			{
+				botTimer.Enabled = false;
+				button1.Text = "Start";
+			}
+		}
+
+		private void startLooping()
+		{
+			if (textBoxIsEmpty())
+			{
+				MessageBox.Show("Please filled up the coordinate");
+				return;
+			}
+			if (!botTimer.Enabled)
+			{
+				botTimer.Interval = Int32.Parse(textBox7.Text);
+				botTimer.Tick += startClicker;
+				button1.Text = "Stop";
+				botTimer.Enabled = true;
+			}
+			else
+			{
+				botTimer.Enabled = false;
+				button1.Text = "Start";
+			}
+		}
+
+		private bool textBoxIsEmpty()
+		{
+			if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "" || textBox7.Text == "")
+			{
+				return true;
+			} else
+			{
+				return false;
+			}
+		}
+
+		private void MoveCursor()
+		{
+			MoveCursor(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+			System.Threading.Thread.Sleep(Int32.Parse(textBox7.Text));
+			mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
+			System.Threading.Thread.Sleep(Int32.Parse(textBox7.Text));
+			MoveCursor(Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text));
+			System.Threading.Thread.Sleep(Int32.Parse(textBox7.Text));
+			mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
+			System.Threading.Thread.Sleep(Int32.Parse(textBox7.Text));
+			MoveCursor(Int32.Parse(textBox5.Text), Int32.Parse(textBox6.Text));
+			System.Threading.Thread.Sleep(Int32.Parse(textBox7.Text));
+			mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
+			//{X=3256,Y=343}
+			//{ X = 2274,Y = 709}
+		}
+
+		private void MoveCursor(int x, int y)
+		{
+			this.Cursor = new Cursor(Cursor.Current.Handle);
+			Cursor.Position = new Point(x, y);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			locateEnable = !locateEnable;
+			if(locateEnable)
+			{
+				toolStripStatusLabel1.Text = "Position to any one of the button";
+				cursorPointTimer.Interval = 5000;
+				cursorPointTimer.Tick += new EventHandler(outputMouseCursor);
+				cursorPointTimer.Enabled = true;
+			}
+		}
+
+		private void outputMouseCursor(object sender, EventArgs e) {
+			cursorPointTimer.Enabled = false;
+			toolStripStatusLabel1.Text = String.Format("x:{0}, y:{1}", System.Windows.Forms.Control.MousePosition.X.ToString(), System.Windows.Forms.Control.MousePosition.Y.ToString());
+		}
+
+		private void startClicker(object sender, EventArgs e)
+		{
+			MoveCursor();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			SetHook();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			UnHook();
+		}
+	}
+}
